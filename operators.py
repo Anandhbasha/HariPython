2afa6bc4dc030116ed7a305e33f59121017361a7
Anandhakumar <anandhaanavamani@gmail.com>
2026-07-15 12:33:22 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,95 @@
+# # arithmetic operator
+# # +,-,*,/,%,//
+
+# print(5+10) #15
+# print(10-5) #5
+# print(5*10) #50
+# print(10/5) #2.0
+# print(10//5) #2
+# print(10%5) #-> 0
+
+# # BODMAS
+# # Bracket
+# # of
+# # Divison
+# # multple
+# # Addtion
+# # Substraction
+
+# print(int((3+5)*3+(6*2)/2//3))
+# # 8*3+12/2//3
+# # 26
+
+# Realational Operators
+# >,<,<=,>=,!=,==
+# print(5>2)
+# print(5<2)
+# print(5<=5)
+# print(5!=5)
+# print(5==5)
+
+# logical operator
+# and or not
+# print(4==4 and 6==6 and 6<3)
+# print(4==4 and 6==6 or 6<3)
+# print(not(5!=5))
+
+# age =20
+# # print("Adult" if age>19 else "Teenager")
+# # membership
+# #list string tuple set dict
+# nums = [1,2,3,4]
+# print(10 not in nums)
+# name = "bala"
+# print('x' in name)
+
+
+# idendity
+# 10->a
+# 10->b
+a = [10,20]
+b= a
+print(a is b)
+
+# bitwise 
+#0,1
+# 2-> rem-0 co -1
+# 10
+#3/2-> rem-1 co-1
+# 11
+#4-> rem -0 co-2 
+# 100
+
+#11
+#rem -1 co-5
+#rem-1 co-2
+#1011
+
+#and
+#1101
+#1011
+#1001
+#1->*2^3->8
+#0->0
+#0->0
+#1 ->*2^0->1
+# 9
+print(5&6)
+#101
+#110
+#100
+#or
+#   1010
+#   0110
+# ->1100
+#xor
+# a^b
+#   101
+#   011
+# ->110
+# not
+#5->-6
+
+#lefshift
+#1101->000001101->->000001011
+#rightshift
